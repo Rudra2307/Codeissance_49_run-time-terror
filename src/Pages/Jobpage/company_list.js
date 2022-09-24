09025b01487fcce64f88053f6898e475b5f1bafb
Rudra2307 <67470013+Rudra2307@users.noreply.github.com>
2022-09-24 09:51:22 +0530

    logo added

---
+++ src/Pages/Jobpage/company_list.js
@@ -0,0 +1,51 @@
+export const company_list = [
+ 
+      {
+        skills: ["react", "node", "mongodb"],
+        application: [],
+        _id: "632e03bdd6d9e311f872bc43",
+        title: "web Development",
+        description: "This is job is about web development",
+        salary: "10000",
+        image: "./images/1.jpg",
+        category: "Technical",
+        __v: 0,
+      },
+      {
+        skills: ["Excel", "PowerPoint"],
+        application: [],
+        _id: "632e05b66bef884b743cf39c",
+        title: "Computer Development",
+        image: "./images/2.png",
+        description: "This is job is about Computer Development",
+        salary: "1000",
+        category: "Technical",
+        __v: 0,
+      },
+      {
+        skills: ["Cooking", "Baking"],
+        application: [],
+        _id: "632e05ea6bef884b743cf39d",
+        title: "Chef",
+        description: "This is job is about Cooking",
+        image: "./images/3.png",
+
+        salary: "7000",
+        category: "Cooking",
+        __v: 0,
+      },
+      {
+        skills: ["Speaking Skills", "Management Skills"],
+        application: [],
+        _id: "632e06386bef884b743cf39e",
+        title: "Assistant Manager",
+        description: "This is job is about Hotel Management",
+        salary: "17000",
+        image: "./images/4.png",
+
+        category: "Management",
+        __v: 0,
+      },
+    
+  
+];
